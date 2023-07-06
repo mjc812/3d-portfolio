6804b9106984333913370975f527453d033b36f3
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-07-05 19:51:04 -0700

    implement prelimary NavBar component to test tailwind and react integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
