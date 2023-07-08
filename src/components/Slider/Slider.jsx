5203dad5286c22ed5ef29b9c1ee3292f018003fb
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-07-07 19:56:34 -0700

    remove scroll-bar

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,7 +3,7 @@ import { data } from './data';
 
 const Slider = () => {
   return (
-    <div className='w-full max-h-60 overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide' id='slider'>
+    <div className='w-full max-h-60 overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide' id='slider' >
         {data.map((item) => (
             <img
                 className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'
