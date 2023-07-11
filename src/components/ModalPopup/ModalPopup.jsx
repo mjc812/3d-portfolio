ae8d2e2f7008566378108f6eeaa2b1cb1e90b97e
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-07-10 21:43:15 -0700

    change font of modal text, and adjust close button and slider padding and margins

--- src/components/ModalPopup/ModalPopup.jsx
+++ src/components/ModalPopup/ModalPopup.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Modal from 'react-modal';
 import Slider from "../Slider";
 import './ModalPopup.css';
+import { styles } from "../../styles";
 
 const modalStyles = {
   content: {
@@ -28,23 +29,38 @@ const ModalPopup = ({ isOpen, onRequestClose, selectedModal, closeModal }) => {
   return (
     <Modal style={modalStyles} isOpen={isOpen} onRequestClose={onRequestClose} contentLabel="Springy Modal" ariaHideApp={false}>
       <div className="flex flex-col">
-        <button className='fixed top-0 right-0 mt-4 mr-4 text-white font-bold py-2 px-4 rounded' onClick={closeModal}>Close</button>
+        <button className='fixed top-0 right-0 mt-4 mr-8 text-white text-[18px] font-bold cursor-pointer flex' onClick={closeModal}>Close</button>
         <div className='w-full min-h-72 max-h-72'>
           <Slider/>
         </div>
-        <div className="mt-5 flex flex-col space-y-5">
+        <div className="flex flex-col">
           {selectedModal === 'Car Rent' && (
             <>
-              <h5 className='text-secondary text-[30px] max-w-3xl leading-[30px]'>Forest 1</h5>
+              <h5 className='font-black text-white lg:text-[40px] sm:text-[30px] xs:text-[30px] text-[30px] lg:leading-[98px]'>Forest 1</h5>
               <div className="flex flex-col md:flex-row space-x-5">
-                <p className='text-secondary text-[15px] max-w-2xl leading-[30px] mb-4 md:mb-0'>
+                <p className='text-secondary text-[17px] max-w-3xl leading-[30px]'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, sem eget fermentum finibus, 
+                  dui justo consectetur erat, non cursus turpis tortor non sapien. In hac habitasse platea dictumst. 
+                  Pellentesque elit lectus, tristique viverra volutpat sed, gravida convallis enim. Maecenas cursus augue 
+                  vel eros cursus interdum. Ut et purus ac ante semper elementum sit amet nec risus. 
+                  Ut faucibus at urna sodales maximus. Sed id consectetur dolor.
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, sem eget fermentum finibus, 
                   dui justo consectetur erat, non cursus turpis tortor non sapien. In hac habitasse platea dictumst. 
                   Pellentesque elit lectus, tristique viverra volutpat sed, gravida convallis enim. Maecenas cursus augue 
                   vel eros cursus interdum. Ut et purus ac ante semper elementum sit amet nec risus. 
                   Ut faucibus at urna sodales maximus. Sed id consectetur dolor.
                 </p>
-                <ul className="list-disc pl-4 text-secondary text-[15px] max-w-3xl">
+                <ul className="list-disc text-secondary text-[17px] max-w-3xl leading-[30px]">
+                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, sem eget fermentum finibus, 
+                  dui justo consectetur erat, non cursus turpis tortor non sapien.</li>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, sem eget fermentum finibus, 
+                  dui justo consectetur erat, non cursus turpis tortor non sapien.</li>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, sem eget fermentum finibus, 
+                  dui justo consectetur erat, non cursus turpis tortor non sapien.</li>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, sem eget fermentum finibus, 
+                  dui justo consectetur erat, non cursus turpis tortor non sapien.</li>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, sem eget fermentum finibus, 
+                  dui justo consectetur erat, non cursus turpis tortor non sapien.</li>
                   <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, sem eget fermentum finibus, 
                   dui justo consectetur erat, non cursus turpis tortor non sapien.</li>
                   <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies, sem eget fermentum finibus, 
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,7 +3,7 @@ import { data } from './data';
 
 const Slider = () => {
   return (
-    <div className='w-full min-h-72 max-h-72 pt-8 pb-4 overflow-x-scroll scroll whitespace-nowrap scroll-smooth' id='slider' >
+    <div className='w-full min-h-72 max-h-72 pt-4 pb-4 overflow-x-scroll scroll whitespace-nowrap scroll-smooth' id='slider' >
         {data.map((item) => (
             <img
                 className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'
