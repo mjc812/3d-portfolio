8b1af921c412934d5dcc73234816c9bc9270533e
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-07-14 18:28:06 -0700

    revove email component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,111 +10,17 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
-
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "JavaScript Mastery",
-          from_email: form.email,
-          to_email: "test@gmail.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
-
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
-  };
-
   return (
     <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
-      <motion.div variants={slideIn("left", "tween", 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl' >
+      <motion.div variants={slideIn("left", "tween", 0.2, 1)}  className='flex-[0.75] bg-black-100 p-8 rounded-2xl flex flex-col items-center justify-center gap-5' >
         <h3 className={styles.sectionHeadText}>Contact.</h3>
-        <div className="mt-3">
-          <p className={styles.sectionSubText}>Email me</p>
-        </div>
-        <form ref={formRef}onSubmit={handleSubmit} className='mt-2 flex flex-col gap-8'>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
-            <input type='text' name='name' value={form.name} onChange={handleChange} placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'/>
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
-            <input type='email' name='email' value={form.email} onChange={handleChange} placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'/>
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
-            <textarea rows={7} name='message' value={form.message} onChange={handleChange} placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'/>
-          </label>
-
-          <button className='bg-tertiary -mt-5 py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary' type='submit'>
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-        <div className="mt-6">
-          <p className={styles.sectionSubText}>Social Media</p>
-          <ul className="mt-3 flex justify-evenly">
-            <li className="social-icon">
-              <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faYoutube} className="text-2xl sm:text-3xl lg:text-4xl" />
-              </a>
-            </li>
+        <p className={styles.sectionSubText}>Email me</p>
+        <p className="text-white-100 text-[14px] pl-1 tracking-wider">michaelchung812@gmail.com</p>
+        <p className={styles.sectionSubText}>Techy Stuff</p>
+          <ul className="flex justify-evenly gap-5">
             <li className="social-icon">
               <a href="https://github.com" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faGithub} className="text-2xl sm:text-3xl lg:text-4xl" />
-              </a>
-            </li>
-            <li className="social-icon">
-              <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} className="text-2xl sm:text-3xl lg:text-4xl" />
-              </a>
-            </li>
-            <li className="social-icon">
-              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faInstagram} className="text-2xl sm:text-3xl lg:text-4xl" />
               </a>
             </li>
             <li className="social-icon">
@@ -123,9 +29,26 @@ const Contact = () => {
               </a>
             </li>
           </ul>
-        </div>
+        <p className={styles.sectionSubText}>Social Media</p>
+        <ul className="flex justify-evenly gap-5">
+          <li className="social-icon">
+            <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faYoutube} className="text-2xl sm:text-3xl lg:text-4xl" />
+            </a>
+          </li>
+          <li className="social-icon">
+            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} className="text-2xl sm:text-3xl lg:text-4xl" />
+            </a>
+          </li>
+          <li className="social-icon">
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} className="text-2xl sm:text-3xl lg:text-4xl" />
+            </a>
+          </li>
+        </ul>
       </motion.div>
-      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'>
+      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className='xl:flex-1 xl:h-550px md:h-[550px] h-[350px]'>
         <EarthCanvas />
       </motion.div>
     </div>
