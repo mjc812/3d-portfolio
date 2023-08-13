554d9323a956cb22f0012e9faa99eb34cbb94b41
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-08-13 04:24:07 -0700

    import boilderplate code from documentation

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Masonry from 'react-masonry-css'
 
 import { SectionWrapper } from "../../hoc";
 
+const breakpointColumnsObj = {
+  default: 4,
+  1100: 3,
+  700: 2,
+  500: 1
+};
+
 const Photos = () => {
   return (
-    <div>Photos</div>
+    <div>
+      <Masonry
+        breakpointCols={3}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column">
+          {/* array of JSX items */}
+      </Masonry>
+    </div>
   )
 }
 
