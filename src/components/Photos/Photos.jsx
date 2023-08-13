e7050ca3d98b02b9b81fdae7b1a9b4f800c8635a
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-08-13 05:34:49 -0700

    re-implement photo gallery using react

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -1,28 +1,37 @@
 import React from 'react'
-import Masonry from 'react-masonry-css'
+import { motion } from "framer-motion";
 
 import { SectionWrapper } from "../../hoc";
 import { data } from './data';
+import { styles } from "../../styles";
+import { textVariant } from "../../utils/motion";
 
 const breakpointColumnsObj = {
   default: 4,
   1100: 3,
-  700: 2,
+  700: 1,
   500: 1
 };
 
 const Photos = () => {
   return (
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="flex ml-[-30px]"
-        columnClassName="masonry-grid-column">
-          {data.map((item, index) => (
-            <div key={index}>
-              <img className='mb-5 w-max inline-block p-2 hover:scale-105 ease-in-out duration-300' src={item.img} />
-            </div>
+    <div>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          Scuba diving, Hiking, Soccer
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Hobbies.
+        </h2>
+      </motion.div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
+        {data.map((item, index) => (
+          <div key={index} className="rounded-md overflow-hidden">
+            <img className='w-full h-auto inline-block p-2 hover:scale-105 ease-in-out duration-300' src={item.img} />
+          </div>
         ))}
-      </Masonry>
+      </div>
+    </div>
   )
 }
 
