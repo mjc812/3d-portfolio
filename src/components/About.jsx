2a12de3058c332b16fda24f72166f25a40580603
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-09-08 22:34:21 -0700

    update about paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,13 +38,10 @@ const About = () => {
       </motion.div>
 
       <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a software engineer with a diverse background with a passion for a relentless pursuit of knowledge and pushing my boundaries. 
+        I take pride in my ability to uncover efficient and creative solutions to real-world problems.
+        I thrive in dynamic environments where I have the opportunity to work closely and collaborate with other talented developers.
       </motion.p>
-
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
