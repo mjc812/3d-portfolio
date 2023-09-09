df08b4f8c69b13771b8ef2807e3881f07f0e65e1
mjc812 <68936604+mjc812@users.noreply.github.com>
2023-09-08 20:47:46 -0700

    rotate computer model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -39,10 +39,11 @@ const ComputersCanvas = () => {
   }, []);
 
   return (
-    <Canvas frameloop='demand' shadows dpr={[1, 2]} camera={{ position: [20, 3, 5], fov: 25 }} 
-      gl={{ preserveDrawingBuffer: true }}>
+    <Canvas frameloop='demand' shadows dpr={[1, 2]} camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
+        
         <OrbitControls
+          autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
